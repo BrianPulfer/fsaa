fix bug that image loss wasn't calculated per image but per whole batch resulting in overflow sometimes

fsaa/losses/mse_loss.py
@@ -5,7 +5,7 @@ from torch.nn import Module, MSELoss
 class MeanSquaredErrorLoss(Module):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.criterion = MSELoss()
+        self.criterion = MSELoss(reduction="none")
 
     def forward(self, x: Tensor, y: Tensor) -> Tensor:
         return self.criterion(x, y)

fsaa/utils.py
@@ -2,7 +2,6 @@ from typing import Callable
 
 from torch import nn
 from torch.nn import Module
-from torch.nn import functional as F
 
 from fsaa.core import (PerceptualMask, PerturbationInitializer,
                        PerturbationUpdater)
@@ -80,10 +79,10 @@ def get_loss(name: str, **kwargs) -> Callable:
     if name in LOSSES:
         return LOSSES[name](**kwargs)
     if hasattr(nn, name):
+        kwargs.update({"reduction": "none"})
         return getattr(nn, name)(**kwargs)
-    if hasattr(F, name):
-        return getattr(F, name)
-    raise ValueError(f"Loss '{name}' is not supported. Pick one of {LOSSES} or use a loss either from torch.nn or torch.nn.functional")
+    raise ValueError(
+        f"Loss '{name}' is not supported. Pick one of {LOSSES} or use a loss either from torch.nn or torch.nn.functional")
 
 
 def get_mask(name: str, **kwargs) -> PerceptualMask:

tests/test_losses.py
@@ -1,7 +1,6 @@
 import pytest
 import torch
 from torch import nn
-from torch.nn import functional as F
 
 from fsaa.utils import LOSSES, get_loss
 
@@ -27,16 +26,5 @@ def test_nn_losses(data):
     assert callable(mse_loss)
     loss = mse_loss(x, y)
     assert isinstance(loss, torch.Tensor)
-    assert torch.allclose(loss, nn.MSELoss()(x, y))
-    assert torch.allclose(loss, (x - y).pow(2).mean())
-
-
-def test_functional_losses(data):
-    x, y = data
-
-    mse_loss = get_loss("mse_loss")
-    assert callable(mse_loss)
-    loss = mse_loss(x, y)
-    assert isinstance(loss, torch.Tensor)
-    assert torch.allclose(loss, F.mse_loss(x, y))
-    assert torch.allclose(loss, (x - y).pow(2).mean())
+    assert torch.allclose(loss.mean(), nn.MSELoss()(x, y))
+    assert torch.allclose(loss.mean(), (x - y).pow(2).mean())