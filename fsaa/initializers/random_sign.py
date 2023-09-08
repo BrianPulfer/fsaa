54749e2e94e9bd2874bdad4d06987d3b72469d36
Brian Pulfer <brian.pulfer@unige.ch>
2023-09-08 14:11:11 +0200

    Added docstrings to initializers.

--- fsaa/initializers/random_sign.py
+++ fsaa/initializers/random_sign.py
@@ -1,11 +1,26 @@
 import torch
+from torch import Tensor
 
 from fsaa.core import PerturbationInitializer
 
 
 class RandomSignInitializer(PerturbationInitializer):
+    """
+    RandomSignInitializer is a PerturbationInitializer that adds random noise to the input.
+    The noise is sampled from a normal distribution and then the sign operator is applied.
+    """
+
     def __init__(self, lr: float = 2 / 255, *args, **kwargs):
         super(RandomSignInitializer, self).__init__(lr, *args, **kwargs)
 
-    def initialize(self, x: torch.Tensor) -> torch.Tensor:
+    def initialize(self, x: Tensor) -> Tensor:
+        """
+        Returns the input with random noise added to it.
+
+        Args:
+            x (Tensor): the input tensor
+
+        Returns:
+            Tensor: the input tensor with random noise added to it
+        """
         return x + torch.randn_like(x).sign() * self.lr
