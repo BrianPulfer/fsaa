8d8d762e702b655161607b8f8873d65f93732a9a
Brian Pulfer <Brian.Pulfer@unige.ch>
2024-04-26 02:01:09 +0200

    Added utility function to plot activations in 3d.

--- fsaa/utils.py
+++ fsaa/utils.py
@@ -1,8 +1,43 @@
 from typing import Tuple
 
+import matplotlib.pyplot as plt
+import numpy as np
 import requests as r
+import torch
 from PIL import Image
 from torchvision.transforms import ToTensor
+
+
+def plot_activations_3d(
+    activations: torch.Tensor,
+    cmap: str = "Greens",
+    abs: bool = True,
+    figsize=(8, 6),
+    wspace=0.0,
+    rstride=1,
+    linewidth=10,
+    label_size=15,
+):
+    feat = activations.squeeze()
+    assert feat.ndim == 2, "Input tensor must be 2D"
+    X, Y = np.meshgrid(np.arange(feat.shape[-2]), np.arange(feat.shape[-1]))
+
+    fig = plt.figure(figsize=figsize)
+    fig.tight_layout()
+    plt.subplots_adjust(wspace=wspace)
+
+    ax = fig.add_subplot(1, 1, 1, projection="3d")
+
+    Z = feat
+    if abs:
+        Z = Z.abs()
+    Z = Z.cpu().numpy().T
+
+    ax.plot_surface(X, Y, Z, rstride=rstride, linewidth=linewidth, cmap=cmap)
+
+    ax.tick_params(axis="x", labelsize=label_size)
+    ax.tick_params(axis="y", labelsize=label_size)
+    ax.tick_params(axis="z", labelsize=label_size)
 
 
 def get_dummy_image(
