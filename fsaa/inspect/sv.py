71becf384053bbd9052c97e1c27d740ea9d11e30
Brian PUlfer <brian.pulfer@unige.ch>
2024-04-29 15:52:43 +0200

    Breaking apart qkv matrices.

--- fsaa/inspect/sv.py
+++ fsaa/inspect/sv.py
@@ -12,9 +12,26 @@ def get_singular_values(model: torch.nn.Module) -> List[Tuple[str, List[float]]]
     Returns:
         A list of tuples, where each tuple contains the name of the layer and a list of singular values.
     """
-    singular_values = []
+    svs = []
     for name, param in model.named_modules():
         if isinstance(param, torch.nn.Linear):
-            sv = torch.svd(param.weight, compute_uv=False).S.detach().tolist()
-            singular_values.append((name, sv))
-    return singular_values
+            # Unpack the weight matrix if it is an attention layer.
+            if 'attn' in name and param.out_features == 3 * param.in_features:
+                order = ["query", "key", "value"]
+                for i in range(3):
+                    sv = (
+                        torch.svd(
+                            param.weight[
+                                i * param.in_features: (i + 1) * param.in_features
+                            ],
+                            compute_uv=False,
+                        )
+                        .S.detach()
+                        .tolist()
+                    )
+                    svs.append((f"{name}_{order[i]}", sv))
+            else:
+                sv = torch.svd(
+                    param.weight, compute_uv=False).S.detach().tolist()
+                svs.append((name, sv))
+    return svs
